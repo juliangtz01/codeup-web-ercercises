6a4b6ab492ec52247f1187fd85b2e18968127fa8
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-08-02 11:22:31 -0500

    Started the Array exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,39 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+var names = ['Angie', 'Bertha', 'Hilda', 'Yolanda'];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+for(var x = 0; x < names.length; x++)
+{
+    console.log(names[x] + " is index " + x);
+}
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for(var x = 0; x < names.length; x++)
+{
+    console.log(names[x]);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+// names.forEach((theNames))
+//     {
+//         console.log(theNames);
+//     }
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +52,26 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function first(itemArray)
+    {
+        console.log(itemArray.indexOf(0));
+    }
+
+    function second(itemArray)
+    {
+        console.log(itemArray.indexOf(1));
+    }
+
+    function last(itemArray)
+    {
+        console.log(itemArray.indexOf(itemArray.length - 1));
+    }
+
+    var myArray = [1,2,3,4,5];
+
+    first(myArray);
+    second(myArray);
+    last(myArray);
 
 })();
