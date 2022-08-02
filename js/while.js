967992c493e8fae3fc075a7c2c2c6bcd403f1c62
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-08-02 08:18:01 -0500

    Completed the While exercise

---
+++ js/while.js
@@ -0,0 +1,29 @@
+/**
+ * 2.
+ * Create a while loop that uses console.log() to create the output shown below
+ *
+ * 2
+ * 4
+ * 8
+ * 16
+ * 32
+ * 64
+ * 128
+ * 256
+ * 512
+ * 1024
+ * 2048
+ * 4096
+ * 8192
+ * 16384
+ * 32768
+ * 65636
+ */
+
+var num = 1;
+
+while(num != 65536)
+{
+    num *= 2;
+    console.log(num);
+}
