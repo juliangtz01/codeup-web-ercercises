bb15d46d7478781d7e54aea0524cad8d00ab42fe
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-08-08 15:56:34 -0500

    Fixed code in the circle.js file

--- js/circle.js
+++ js/circle.js
@@ -8,21 +8,31 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
+            //var area = Math.PI * Math.pow(circle.radius, 2);
             return Math.PI * Math.pow(circle.radius, 2); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
 
+            let newArea;
+
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
             if(doRounding)
             {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+                //console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+                newArea = Math.round(this.getArea());
             }
             else
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+            {
+                //console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+                newArea = this.getArea();
+
+            }
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + newArea);
         }
+
     };
 
     // log info about the circle
