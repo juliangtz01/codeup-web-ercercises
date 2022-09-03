600698f00ae5de8171cf0954250e1e4edc209aa9
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-09-02 21:15:54 -0500

    practicing jQuery

---
+++ js/document.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// window.onload = $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+
+$('*').css('border', '1px solid #F00');
+
+// var x = ($('p').text());
+//
+// alert(typeof x);
+//
+// alert(x);
+
+$('li').css('background-color', 'yellow').css('display', 'inline');
+
+
+
+
