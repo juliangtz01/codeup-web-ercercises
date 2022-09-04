38ac73af5ff2af46b2ee9e02ebc21b862f08439f
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-09-04 17:54:18 -0500

    adding const values to the weather practice project

---
+++ Coding_Practice/Weather_App_Practice/script.js
@@ -0,0 +1,6 @@
+const timeE1 = document.getElementById('time');
+const dateE1 = document.getElementById('date');
+const currentWeatherItemsE1 = document.getElementById('current-weather-items');
+const timeZone = document.getElementById('time-zone');
+const countryE1 = document.getElementById('country');
+const weatherForecastE1 = document.getElementById('weather');
